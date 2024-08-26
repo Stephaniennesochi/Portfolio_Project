2d555f90584da508576a3d86aa802fe56cc6a364
Stephaniennesochi <ezestephanie64@gmail.com>
2024-08-26 17:17:42 +0100

    portfolio

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const allSections = document.querySelector('.main-content');
 
 function pageTransitions(){
     // Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
+    for (let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
